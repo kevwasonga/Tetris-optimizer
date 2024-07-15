044e9f3d2a9d79eec20f1da41af76a389005a948
kevwasonga <kevinwasonga116@gmail.com>
2024-07-15 23:44:13 +0300

    import new functions

--- main.go
+++ main.go
@@ -12,15 +12,15 @@ func main() {
 	bannerMap, err := tetromino.Loadbanner(fileName)
 	if err != nil {
 		log.Fatalf("Error loading banner: %v", err)
+		return
 	}
 
 	validTetrominoes := make(map[int]string)
 	for key, tetrominoStr := range bannerMap {
 		// Validate the tetromino
 		if tetromino.IsValidTetromino(tetrominoStr) {
-			// repace the # with  their respective latin equivalents
+			// Replace the # with their respective Latin equivalents
 			validTetrominoes[key] = tetromino.ReplaceChars(tetrominoStr, key)
-			//	validTetrominoes[key] = tetrominoStr
 		} else {
 			fmt.Printf("Tetromino %d is invalid.\n", key)
 		}
@@ -31,42 +31,18 @@ func main() {
 
 	// Print assembled square
 	fmt.Println("Assembled Tetrominoes into Square:")
-	printAssembled(square, validTetrominoes)
+	printAssembled(square)
 }
 
-func printAssembled(square [][]rune, validTetrominoes map[int]string) {
-	// Calculate the minimum square size based on the number of valid tetrominoes
-	size := calculateMinimumSquareSize(len(validTetrominoes))
-
-	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
-			char := '.'
-			if i < len(square) && j < len(square[i]) {
-				char = square[i][j]
+func printAssembled(square [][]rune) {
+	for _, row := range square {
+		for _, cell := range row {
+			if cell == ' ' {
+				fmt.Print(" ") // Print '.' for empty cells
+			} else {
+				fmt.Print(string(cell)) // Print the tetromino character
 			}
-			fmt.Print(string(char))
 		}
 		fmt.Println()
 	}
-}
-
-func calculateMinimumSquareSize(numTetrominoes int) int {
-    // Calculate the total number of cells required to accommodate all tetrominoes
-    requiredCells := numTetrominoes * 16 // Each tetromino is 4x4 = 16 cells
-
-    // Calculate the initial smallest square size that can fit all tetrominoes
-    size := 1// int(math.Ceil(math.Sqrt(float64(requiredCells))))
-
-    // Increment the size until it can accommodate all tetrominoes
-    for {
-        totalCells := size * size
-        if totalCells >= requiredCells {
-            break
-        }
-        size++
-    }
-
-    return size
-
-
 }
